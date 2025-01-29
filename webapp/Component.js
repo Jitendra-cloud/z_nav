8fe10b1e10347e66355da550eb61e93d8fbc055d
Jitendra Singh <jhonygautam@gmail.com>
2025-01-29 09:42:53 +0000

    Init

--- webapp/Component.js
+++ webapp/Component.js
@@ -4,8 +4,16 @@ sap.ui.define([
     function (UIComponent) {
         "use strict";
 
-        return UIComponent.extend("sap.training.exc.Component", {
+        return UIComponent.extend("djp.Component", {
 
+            metadata: {
+                manifest: "json"
+            },
+
+            init: function () {
+                // call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments);
+            }
 
 
 
--- webapp/index.js
+++ webapp/index.js
@@ -1,11 +1,23 @@
-sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
+//sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
+sap.ui.define(["sap/ui/core/ComponentContainer"], function (ComponentContainer) {
   "use strict";
 
-  XMLView.create({
-    id: "App",
-    viewName: "djp.view.App"
-  }).then(function (oView) {
-    oView.placeAt("content");
+  // XMLView.create({
+  //   id: "App",
+  //   viewName: "djp.view.App"
+  // }).then(function (oView) {
+  //   oView.placeAt("content");
+  // });
+
+  var oContainer = new ComponentContainer({
+    id: "container",
+    name: "djp",
+    manifest: true,
+    async: true,
+    settings: {
+      id: "djp"
+    }
   });
+  oContainer.placeAt("content");
 
 });
