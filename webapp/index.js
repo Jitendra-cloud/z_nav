adf15c35b66febc20a5c276bc59445d02478918e
Jitendra Singh <jhonygautam@gmail.com>
2025-01-29 08:13:48 +0000

    Completed

--- webapp/index.js
+++ webapp/index.js
@@ -1,14 +1,11 @@
-sap.ui.define(["sap/m/Text"], function (Text) {
+sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
   "use strict";
 
-  new Text({
-    text: "Hello World 2,  Hello World 2,  Hello World 3,  Hello World 4,  Hello World 5,  Hello World 6", width: "20%",
-    maxLines: 2, textAlign: sap.ui.core.TextAlign.Center
-  }).placeAt("content");
-
-  new Text({
-    text: "Hello World 7,  Hello World 8,  Hello World 9,  Hello World 10,  Hello World 11,  Hello World 12", width: "20%",
-    maxLines: 2, textAlign: sap.ui.core.TextAlign.Center
-  }).placeAt("content");
+  XMLView.create({
+    id: "App",
+    viewName: "djp.view.App"
+  }).then(function (oView) {
+    oView.placeAt("content");
+  });
 
 });
